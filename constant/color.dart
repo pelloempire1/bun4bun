ce5e9ed5410878116d047ca8fabdd78ca9f942b5
pelloempire1 <107160219+pelloempire1@users.noreply.github.com>
2025-12-12 23:58:19 -0500

    Delete constant directory

--- constant/color.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color grey = Color(0xfff8f5ff);
-const Color mint = Color(0xffe7ffb6);
-const Color dark = Color(0xff0f172a);
-const Color red = Color(0xffff4b4b);
-const Color green = Color(0xff22c55e);
-const Color yellow = Color(0xfffacc15);
